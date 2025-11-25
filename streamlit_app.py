1732d254798f70ea15060b5070e65ab70678dc36
N Hameleers <niels.hameleers@gmail.com>
2025-11-25 11:24:54 +0000

    added loading dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,37 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# OPEN/LOAD DATA
+cvd = pd.read_csv('https://raw.githubusercontent.com/LUCE-Blockchain/Databases-for-teaching/refs/heads/main/Framingham%20Dataset.csv')
+
+# st.dataframe(cvd.head())
+
+x = st.slider('description')  # 👈 this is a widget
+
+st.write(x, 'squared is', x * x)
+
+iso_code = st.selectbox('these are iso codes', options=['NLD', 'CAN'])
+
+st.write(iso_code)
+# CLEAN/EXPLORE/MANIPULATE DATA
+
+
+
+
+# SHOW FANCY VIZUALIZATIONS
+
+
+
+
+
+
+'# Heading 1'
+
+
+'## Heading 2'
